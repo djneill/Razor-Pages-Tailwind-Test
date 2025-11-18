7c1423101613946b1fd158db0d1eca7eb21b700f
DJ Neill <dj.neill856@gmail.com>
2025-11-18 13:33:19 -0600

    first commit

--- RazorAddressBook/Pages/Address.cshtml.cs
+++ RazorAddressBook/Pages/Address.cshtml.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace RazorAddressBook.Pages
 {
+    [BindProperties]
     public class Address : PageModel
     {
-        private readonly ILogger<Address> _logger;
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public List<string> AddressList { get; set; } = new List<string>();
 
+        private readonly ILogger<Address> _logger;
         public Address(ILogger<Address> logger)
         {
             _logger = logger;
@@ -20,5 +27,14 @@ namespace RazorAddressBook.Pages
         public void OnGet()
         {
         }
+
+        public IActionResult OnPost()
+        {
+            string fullAddress = $"{StreetAddress} {City} {State} {Zipcode}";
+            AddressList.Add(fullAddress);
+            return Page();
+        }
+
+
     }
 }
